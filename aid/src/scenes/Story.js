713172b980603e6741887a8e4c3db2e71d6aa870
Jeffrey Tong <jeffrey.w.tong@gmail.com>
2025-01-12 22:33:55 -0500

    Finish Story scene

--- aid/src/scenes/Story.js
+++ aid/src/scenes/Story.js
@@ -5,84 +5,69 @@ function Story() {
         this.background = storyPage >= 5 ? 192 : 255;
         fill(0);
         textSize(30);
-        if ('16'.includes(storyPage)) {
-            textAlign(CENTER, CENTER);
+        if ([1, 6].includes(storyPage)) {
             textStyle(ITALIC);
+            textAlign(CENTER, CENTER);
             textSize(36);
+            imageMode(CENTER);
+            rectMode(CENTER);
         } else {
             textAlign(LEFT, TOP);
-            textSize(24);
+            textSize(18);
         }
         
+        noStroke();
         switch (storyPage) {
             case 1:
-                tint(255, 25);
-                image(images.openingScene, 0, 0);
-                tint(255, 255);
-                textAlign(CENTER, CENTER);
-                rectMode(CENTER);
-                text(`Our story begins with Milo's adventures in the Kingdom of Digitopolis, ruled by the Mathemagician and hidden away in the vast Lands Beyond...`, 480, 240, 600);
+                text(`Our story begins with Milo's adventures in the Kingdom of Digitopolis, ruled by the Mathemagician and hidden away in the vast Lands Beyond...`, INTRINSIC_MAIN_S / 2, 240, INTRINSIC_MAIN_S);
                 textSize(24);
-                text(`Excerpted from "Childcraft: The How and Why Library",\nVolume 13 (©1985), p. 93–113`, 480, 390);
-                rectMode(CORNER);
-                
+                text(`Excerpted from "Childcraft: The How and Why Library", Volume 13 (©1985), p. 93–113`, INTRINSIC_MAIN_S / 2, 390, INTRINSIC_MAIN_S);
                 break;
             case 2:
-                text(`\t"Can you show me the biggest number there is?" asked Milo.
-\t"I'd be delighted," replied the Mathemagician, opening one of the closet doors. "We keep it right here. It took four miners just to dig it out."
-\tInside was the biggest
+                text(`"Can you show me the biggest number there is?" asked Milo.
 
+"I'd be delighted," replied the Mathemagician, opening one of the closet doors. "We keep it right here. It took four miners just to dig it out."
 
-
-
-
-
-
-
-
-
-
-Milo had ever seen. It was fully twice as high as the Mathemagician.`, 80, 80, 800);
-                image(images.storyline[0], 80, 200, 318, 304);
+Inside was the biggest`, 0, 40, INTRINSIC_MAIN_S);
+                image(images.storyline[0], 0, 180, 318, 304);
+                text(`Milo had ever seen. It was fully twice as high as the Mathemagician.`, 0, 500, INTRINSIC_MAIN_S);
                 break;
             case 3:
-                text(`	"No, that's not what I mean," objected Milo. "Can you show me the longest number there is?"
+                text(`"No, that's not what I mean," objected Milo. "Can you show me the longest number there is?"
+
 "Surely," said the Mathemagician, opening another door. "Here it is. It took three carts to carry it here."
-Inside this closest was the longest
 
-
-
-
-
-
-
-
-
-
-
-imaginable. It was just about as wide as the three was high.`, 80, 80, 800);
-                image(images.storyline[1], 80, 220, 640, 320);
+Inside this closest was the longest`, 0, 40, INTRINSIC_MAIN_S);
+                image(images.storyline[1], 0, 200, 620, 320);
+                text(`imaginable. It was just about as wide as the three was high.`, 0, 520, INTRINSIC_MAIN_S);
                 break;
             case 4:
-                text(`	"No, no, no, that's not what I mean either," he said, looking helplessly at Tock.
+                text(`"No, no, no, that's not what I mean either," he said, looking helplessly at Tock.
+
 "I think what you would like to see," said the dog, scratching himself just under half-past four, "is the number of greatest possible magnitude."
+
 "Well, why didn't you say so?" said the Mathemagician, who was busily measuring the edge of a raindrop.
+
 ...
-"Just follow that line forever," said the Mathemagician, "and when you reach the end, turn left. There you'll find the land of Infinity, where the tallest, the shortest, the biggest, the smallest, and the most and the least of everything are kept."`, 80, 80, 800);
+
+"Just follow that line forever," said the Mathemagician, "and when you reach the end, turn left. There you'll find the land of Infinity, where the tallest, the shortest, the biggest, the smallest, and the most and the least of everything are kept.
+
+..."`, 0, 40, INTRINSIC_MAIN_S);
                 break;
             case 5:
-                text(`...
-Milo bounded across the room and started up the stairs two at a time. "Wait for me, please," he shouted to Tock and the Humbug. "I'll be gone just a few minutes."`, 80, 80, 600);
-                image(images.storyline[2], 708, 30, 192, 544);
+                text(`Milo bounded across the room and started up the stairs two at a time. "Wait for me, please," he shouted to Tock and the Humbug. "I'll be gone just a few minutes."`, 0, 40, INTRINSIC_MAIN_S - 216);
+                image(images.storyline[2], INTRINSIC_MAIN_S - 192, 40, 192, 544);
                 break;
             case 6:
                 text(`Of course, Milo could never reach Infinity, for he is only a mortal.
-But you are no mere mortal. You are Captain Zero; it is your destiny to meet the immortal Infinitus.`, 120, 180, 720);
-                image(images.characters[0], 480 - (180 * images.CAPTAIN_ZERO_RATIO) / 2, 360, 180 * images.CAPTAIN_ZERO_RATIO, 180);
+
+But you are no mere mortal. You are Captain Zero; it is your destiny to meet the immortal Infinitus.`, INTRINSIC_MAIN_S / 2, 180, INTRINSIC_MAIN_S);
+                image(images.characters[0], INTRINSIC_MAIN_S / 2, INTRINSIC_MAIN_S * 0.6, 180 * images.CAPTAIN_ZERO_RATIO, 180);
                 break;
             default:
                 mgr.showScene(Game);
         }
+        rectMode(CORNER);
     };
 
     this.buttons = [
